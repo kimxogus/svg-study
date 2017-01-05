a62369e3cc3f5a0c3ec378598ad79e79d19a6296
kimxogus <kgyoo8232@gmail.com>
2017-01-05 13:26:12 +0900

    add d3, d3-zoomer import and data

--- bbox/script.js
+++ bbox/script.js
@@ -1,1 +1,13 @@
-console.log("hi");
+var data = [{
+    name: 'NODE1',
+    x: 200,
+    y: 300
+}, {
+    name: 'NODE2',
+    x: 200,
+    y: 100
+}, {
+    name: 'NODE3',
+    x: 400,
+    y: 200
+}];
